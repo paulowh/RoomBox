d2de3fc33c008d632abaf5f49a1e36f423b2f2ea
Paulo Santos <paulo.whsantos@hotmail.com>
2025-08-27 11:44:37 -0300

    configurando o arquivo .env

--- config.php
+++ config.php
@@ -1,6 +1,11 @@
 <?php
-$dsn = 'mysql:dbname=u687609827_teste;host=193.203.175.83';
-$usuario = 'u687609827_paulowh';
-$senha = 'S12Oxx7X3';
+$_ENV = parse_ini_file('.env');
+
+var_dump($_ENV);
+
+echo '<hr>';
+$dsn = "mysql:dbname={$_ENV['BANCO']};host={$_ENV['HOST']}";
+$usuario = $_ENV['USUARIO'];
+$senha = $_ENV['SENHA'];
 
 $conn = new PDO($dsn, $usuario, $senha);
