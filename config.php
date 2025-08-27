e4b8538b079743b20c71c1a146d62deb5d4215bf
Paulo Santos <paulo.whsantos@hotmail.com>
2025-08-27 11:20:22 -0300

    consulta das reservas no banco

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+$dsn = 'mysql:dbname=db_gerenciador_sala;host=localhost';
+$usuario = 'root';
+$senha = '';
+
+$conn = new PDO($dsn, $usuario, $senha);
--- template/header.php
+++ template/header.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 
+<?php require_once './config.php'; ?>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
